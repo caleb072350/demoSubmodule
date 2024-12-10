bbf8092e63c9078dc5757f4bd8c747d92054d08e
caleb072350 <caleb072350@gmail.com>
2024-12-10 16:26:17 +0800

    使用abseil库进行字符串拼接

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 #include "mymodule/mymodule.cpp" // 包含自定义模块
 
 int main() {
-    std::cout << "Hello from demo!" << std::endl;
+    std::string str1 = "Hello";
+    std::string str2 = "Abseil";
+    std::string str3 = "!";
+    // 使用 absl::StrCat 进行字符串拼接
+    std::string result = absl::StrCat(str1, ", ", str2, str3);
+    std::cout << result << std::endl;
     mymodule_function(); // 调用自定义函数
     return 0;
 }
--- src/mymodule/mymodule.cpp
+++ src/mymodule/mymodule.cpp
@@ -2,5 +2,10 @@
 #include "absl/strings/str_join.h" // 使用 Abseil 的字符串库
 
 void mymodule_function() {
-    std::cout << "Hello from mymodule!" << std::endl;
+    std::string str1 = "Hello";
+    std::string str2 = "from";
+    std::string str3 = "mymodule!";
+
+    std::string result = absl::StrCat(str1, " ", str2, " ", str3);
+    std::cout << result << std::endl;
 }
